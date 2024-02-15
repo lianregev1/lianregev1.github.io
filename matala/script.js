bb3179cf13385e7995d54c5565cc4325dcacbe2a
LianRegev <lianregev22@gmail.com>
2024-02-15 15:42:57 +0200

    Create script.js

---
+++ matala/script.js
@@ -0,0 +1,9 @@
+function logHi (){
+    console.log ("Hello you!") ;
+}
+
+function divClicked (){
+    alert(document.getElementById("dorothy").innerHTML);
+    document.getElementById("dorothy").style.left= '300px';
+    document.getElementById("dorothy").style.top= '400px';
+}
